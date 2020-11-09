13779a9f8ec34f648dde3e15c1a5c83b92dc0837
root <soumojyotiganguly270@gmail.com>
2020-11-09 11:23:51 +0530

    get lat lon

--- app/src/main/java/com/example/getlocation/MainActivity.java
+++ app/src/main/java/com/example/getlocation/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.getlocation;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.app.AlertDialog;
@@ -10,6 +11,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.provider.Settings;
 import android.os.Bundle;
@@ -22,17 +24,42 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final int REQUEST_LOCATION = 1;
-    Button btnGetLocation;
-    TextView showLocation;
-    LocationManager locationManager;
-    String latitude, longitude;
+    private GpsTracker gpsTracker;
+    private TextView tvLatitude,tvLongitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        tvLatitude = (TextView)findViewById(R.id.latitude);
+        tvLongitude = (TextView)findViewById(R.id.longitude);
+
+        try {
+            View view = getWindow().getDecorView().findViewById(android.R.id.content);
+
+            if (ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ) {
+                ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 101);
+            }
+            getLocation(view);
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+    public void getLocation(View view){
+        gpsTracker = new GpsTracker(MainActivity.this);
+        if(gpsTracker.canGetLocation()){
+            double latitude = gpsTracker.getLatitude();
+            double longitude = gpsTracker.getLongitude();
+            tvLatitude.setText(String.valueOf(latitude));
+            tvLongitude.setText(String.valueOf(longitude));
+        }else{
+            gpsTracker.showSettingsAlert();
+        }
     }
 
 }
